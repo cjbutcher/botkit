8758da8625c6b72023d18795bd9c51ce819d9492
cjbutcher <christopherbutcher@live.com>
2016-03-15 17:17:22 +0000

    Initial commit

--- bot.js
+++ bot.js
@@ -1,68 +1,4 @@
-/*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-           ______     ______     ______   __  __     __     ______
-          /\  == \   /\  __ \   /\__  _\ /\ \/ /    /\ \   /\__  _\
-          \ \  __<   \ \ \/\ \  \/_/\ \/ \ \  _"-.  \ \ \  \/_/\ \/
-           \ \_____\  \ \_____\    \ \_\  \ \_\ \_\  \ \_\    \ \_\
-            \/_____/   \/_____/     \/_/   \/_/\/_/   \/_/     \/_/
-
-
-This is a sample Slack bot built with Botkit.
-
-This bot demonstrates many of the core features of Botkit:
-
-* Connect to Slack using the real time API
-* Receive messages based on "spoken" patterns
-* Reply to messages
-* Use the conversation system to ask questions
-* Use the built in storage system to store and retrieve information
-  for a user.
-
-# RUN THE BOT:
-
-  Get a Bot token from Slack:
-
-    -> http://my.slack.com/services/new/bot
-
-  Run your bot from the command line:
-
-    token=<MY TOKEN> node bot.js
-
-# USE THE BOT:
-
-  Find your bot inside Slack to send it a direct message.
-
-  Say: "Hello"
-
-  The bot will reply "Hello!"
-
-  Say: "who are you?"
-
-  The bot will tell you its name, where it running, and for how long.
-
-  Say: "Call me <nickname>"
-
-  Tell the bot your nickname. Now you are friends.
-
-  Say: "who am I?"
-
-  The bot will tell you your nickname, if it knows one for you.
-
-  Say: "shutdown"
-
-  The bot will ask if you are sure, and then shut itself down.
-
-  Make sure to invite your bot into other channels using /invite @<my bot>!
-
-# EXTEND THE BOT:
-
-  Botkit is has many features for building cool and useful bots!
-
-  Read all about it here:
-
-    -> http://howdy.ai/botkit
-
-~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
-
+var charlie = require("./charlie.js");
 
 if (!process.env.token) {
     console.log('Error: Specify token in environment');
@@ -80,108 +16,46 @@ var bot = controller.spawn({
     token: process.env.token
 }).startRTM();
 
-
-controller.hears(['hello','hi'],'direct_message,direct_mention,mention',function(bot, message) {
-
-    bot.api.reactions.add({
-        timestamp: message.ts,
-        channel: message.channel,
-        name: 'robot_face',
-    },function(err, res) {
-        if (err) {
-            bot.botkit.log('Failed to add emoji reaction :(',err);
-        }
-    });
-
-
-    controller.storage.users.get(message.user,function(err, user) {
-        if (user && user.name) {
-            bot.reply(message,'Hello ' + user.name + '!!');
-        } else {
-            bot.reply(message,'Hello.');
-        }
-    });
+controller.hears(['whoami'], 'direct_message,direct_mention,mention', function(bot, message) {
+  charlie.get(bot, message, 'whoami')
 });
 
-controller.hears(['call me (.*)'],'direct_message,direct_mention,mention',function(bot, message) {
-    var matches = message.text.match(/call me (.*)/i);
-    var name = matches[1];
-    controller.storage.users.get(message.user,function(err, user) {
-        if (!user) {
-            user = {
-                id: message.user,
-            };
-        }
-        user.name = name;
-        controller.storage.users.save(user,function(err, id) {
-            bot.reply(message,'Got it. I will call you ' + user.name + ' from now on.');
-        });
-    });
+controller.hears(['how much holiday have i used?'], 'direct_message', function(bot, message) {
+  charlie.get(bot, message, 'leave_requests/days_used')
 });
 
-controller.hears(['what is my name','who am i'],'direct_message,direct_mention,mention',function(bot, message) {
-
-    controller.storage.users.get(message.user,function(err, user) {
-        if (user && user.name) {
-            bot.reply(message,'Your name is ' + user.name);
-        } else {
-            bot.reply(message,'I don\'t know yet!');
-        }
-    });
+controller.hears(['who is off right now?'], 'direct_message', function(bot, message) {
+  charlie.get(bot, message, 'leave_requests/away_now')
 });
 
-
-controller.hears(['shutdown'],'direct_message,direct_mention,mention',function(bot, message) {
-
-    bot.startConversation(message,function(err, convo) {
-
-        convo.ask('Are you sure you want me to shutdown?',[
-            {
-                pattern: bot.utterances.yes,
-                callback: function(response, convo) {
-                    convo.say('Bye!');
-                    convo.next();
-                    setTimeout(function() {
-                        process.exit();
-                    },3000);
-                }
-            },
-        {
-            pattern: bot.utterances.no,
-            default: true,
-            callback: function(response, convo) {
-                convo.say('*Phew!*');
-                convo.next();
-            }
-        }
-        ]);
-    });
+controller.hears(['sick today'], 'direct_message', function(bot,message) {
+  charlie.bookSickDay(bot, message, 'sick_days/create')
 });
 
-
-controller.hears(['uptime','identify yourself','who are you','what is your name'],'direct_message,direct_mention,mention',function(bot, message) {
-
-    var hostname = os.hostname();
-    var uptime = formatUptime(process.uptime());
-
-    bot.reply(message,':robot_face: I am a bot named <@' + bot.identity.name + '>. I have been running for ' + uptime + ' on ' + hostname + '.');
-
+controller.hears(['book time off'], 'direct_message', function(bot,message) {
+  bot.startConversation(message, askStartDate)
 });
 
-function formatUptime(uptime) {
-    var unit = 'second';
-    if (uptime > 60) {
-        uptime = uptime / 60;
-        unit = 'minute';
-    }
-    if (uptime > 60) {
-        uptime = uptime / 60;
-        unit = 'hour';
-    }
-    if (uptime != 1) {
-        unit = unit + 's';
-    }
+askStartDate = function(response, convo) {
 
-    uptime = uptime + ' ' + unit;
-    return uptime;
+  convo.ask("Ok cool. What is the start date of this time off?", function(response, convo) {
+    askDuration(response, convo);
+    convo.next();
+  }, 
+  { key: 'start_date'});
+}
+
+askDuration = function(response, convo) {
+  convo.ask("Great. Is this just for the 'morning', 'afternoon' or 'one day'? If longer, just tell me the end date.", function(response, convo) {
+    askDetails(response, convo);
+    convo.next();
+  },
+  { key: 'end_date'});
+}
+
+askDetails = function(response, convo) { 
+  convo.ask("Would you like to include some details with this request? If so, type it here. Otherwise just say 'no'!", function(response, convo) {
+    charlie.bookLeave(convo);
+  },
+  { key: 'details'});
 }
---
+++ charlie.js
@@ -0,0 +1,78 @@
+var request = require('request');
+var accessToken = '741a4d49fae74febf12ef756c84b41ffdd51a0645fc88987ca95faea2a5f693d'
+
+exports.get = function(bot, message, endpoint) {
+
+	var url = compileUrl(message.user, endpoint)
+
+  request(url, function(error, response, body) {
+
+	  if (response.statusCode === 401) {
+	  	bot.startPrivateConversation(message, function(err,convo) {
+		    convo.ask("Hey! :simple_smile: It looks like you are trying to talk to me, but I can't be sure who you are yet. What is your Charliebot password?", function(message, convo) {
+		    	authenticate(bot, message);
+		    	convo.next();
+		    });
+		  });
+	    return
+	  }
+	  
+	  if (!error && response.statusCode == 200) {
+	    bot.reply(message, formatText(body));
+	  }
+
+  });
+
+}
+
+exports.bookLeave = function(convo) {
+
+	var slack_user_id = convo.responses.start_date.user
+
+	var url = compileUrl(slack_user_id, 'leave_requests/create') +
+																			'&start_date=' + convo.extractResponse('start_date') +
+																			'&end_date=' + convo.extractResponse('end_date') +
+																			'&details=' + convo.extractResponse('details')
+
+
+	request.post(url, function (error, response, body) {
+		convo.say(formatText(body));
+		convo.next();
+  });
+}
+
+exports.bookSickDay = function(bot, message, endpoint) {
+
+	var url = compileUrl(message.user, endpoint)
+
+  request.post(url, function (error, response, body) {
+	  bot.reply(message, formatText(body));
+	});
+
+}
+
+function compileUrl(slack_user_id, endpoint) {
+	return 'http://charlie.dev/api/v1/' + slack_user_id + '/' + endpoint + '?' + 	'access_token=' + accessToken
+}
+
+function formatText(body) {
+	return JSON.parse(body).replace(/^"(.*)"$/, '$1');
+}
+
+function authenticate(bot, message) {
+  bot.api.users.info({ 'user' : message.user }, function(err, res) {
+    //email = res.user.profile.email
+    email = 'chris@theeleven.co.uk'
+
+    var url = compileUrl(message.user, 'authenticate') +
+    										"&email=" + email + 
+    										"&charliebot_password=" + message.text
+
+    request.post(url, function (error, response, body) {
+      if (!error && response.statusCode == 200) {
+        console.log(body)
+      }
+    });
+  });
+}
+
